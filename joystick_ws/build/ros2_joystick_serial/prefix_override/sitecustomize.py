5b09cadb2acd4b447c5a788b9c9bf6571e69536c
Marco Bruderer <brumarco@ethz.ch>
2025-03-27 20:49:49 +0100

    Removed unnecassary building folders from git repo and modified .gitignore

--- joystick_ws/build/ros2_joystick_serial/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/parallels/Documents/hasel-engine-controller/joystick_ws/install/ros2_joystick_serial'
